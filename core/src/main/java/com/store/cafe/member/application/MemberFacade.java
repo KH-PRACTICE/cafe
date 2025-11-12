882bcf889e0643c8a0956584d1ca1724d41fc5a7
kihyuk.jeong <kihyuk.jeong@musinsa.com>
2025-11-13 02:20:56 +0900

    add

--- core/src/main/java/com/store/cafe/member/application/MemberFacade.java
+++ core/src/main/java/com/store/cafe/member/application/MemberFacade.java
@@ -8,9 +8,25 @@ import com.store.cafe.member.application.result.MemberWithdrawalResult;
 
 public interface MemberFacade {
 
+    /**
+     * 회원가입
+     * @param command (loginId, password, name, phone, gender, birth)
+     * @return MemberSignupResult (memberUid, loginIdHash)
+     */
     MemberSignupResult signup(MemberSignupCommand command);
 
+    /**
+     * 회원탈퇴 요청
+     * @param memberUid 회원고유번호
+     * @param command (reason, requestedAt)
+     * @return MemberWithdrawalResult (memberUid, loginIdHash, withdrawalRequestedAt)
+     */
     MemberWithdrawalResult withdrawal(Long memberUid, MemberWithdrawalCommand command);
 
+    /**
+     * 회원탈퇴 요청 취소
+     * @param memberUid 회원고유번호
+     * @return MemberWithdrawalCancelResult (memberUid, loginIdHash, withdrawalCanceledAt)
+     */
     MemberWithdrawalCancelResult cancelWithdrawal(Long memberUid);
 }
--- core/src/main/java/com/store/cafe/order/application/facade/OrderFacade.java
+++ core/src/main/java/com/store/cafe/order/application/facade/OrderFacade.java
@@ -6,7 +6,18 @@ import com.store.cafe.order.application.result.OrderResult;
 
 public interface OrderFacade {
 
+    /**
+     * 주문 요청
+     * @param command (memberUid, items)
+     * @return OrderResult (orderId, totalAmount, status, orderedAt)
+     */
     OrderResult order(OrderCommand command);
 
+
+    /**
+     * 주문 취소
+     * @param orderId, memberUid
+     * @return OrderCancelResult (orderId, success, status, canceledAt)
+     */
     OrderCancelResult cancelOrder(Long orderId, Long memberUid);
 }
